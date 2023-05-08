ddbbe74015118e5651a1ddd18122fe03c6ff741a
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2023-05-08 10:38:38 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=671443ea3db9f63d50b55142fadb7ca3&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=671443ea3db9f63d50b55142fadb7ca3&query="'
+
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const main = document.getElementById('main')
+
+//get initial movies
+getMovies(API_URL)
+
+async function getMovies(url){
+    const res = await fetch(url)
+    const data = await res.json()
+
+    showMovies(data.results)
+}
+
+function showMovies(movies){
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const {title, poster_path, vote_average, overview} = movie
+
+        const movieElem = document.createElement('div')
+        movieElem.classList.add('movie')
+        movieElem.innerHTML = `
+         <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+             <h3>${title}</h3>
+             <span class="${ratingColor(vote_average)}">${vote_average.toFixed(1)}</span>
+         </div>
+         <div class="overview">
+             <h3>Overview</h3>
+             ${overview}
+            </div>
+        `
+        main.appendChild(movieElem)
+    })
+}
+
+function ratingColor(vote){
+    if(vote >= 8){
+        return 'green'
+    }
+    else if(vote >= 5){
+        return 'orange'
+    }
+    else{
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e) =>{
+
+    e.preventDefault()
+
+    const searchTerm = search.value 
+
+    if(searchTerm && searchTerm !== ''){
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
+    }
+    else{
+        window.location.reload()
+    }
+
+})
